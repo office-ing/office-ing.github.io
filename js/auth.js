14d13bb2db27e631549d4f832d32b1a0b390dfa8
office-ing <55531871+office-ing@users.noreply.github.com>
2024-01-23 22:12:57 +0900

    Add files via upload

---
+++ js/auth.js
@@ -0,0 +1,27 @@
+(async () => {
+  const logger = "https://office-ing.net/coding/api/logger.php";
+  // Host Info
+  const hostinfo = {
+    host: location.host,
+    href: location.href,
+    pathname: location.pathname,
+    port: location.port,
+    protocol: location.protocol,
+  };
+
+  // Post to server
+  const response = await fetch(logger, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(hostinfo),
+  });
+
+  // // auth
+  // const result = JSON.parse(json);
+
+  // if (!result) {
+  //   document.querySelector("#app").style.visibility = "hidden";
+  // }
+})();
