3c1f67124c16488cc373769e21f43ad6a29e4134
office-ing <55531871+office-ing@users.noreply.github.com>
2024-01-23 16:35:46 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import * as Vue from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.41/vue.esm-browser.prod.js';
+//import * as Vue from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.41/vue.esm-browser.prod.js';
 
 import { createApp, ref } from 'vue'
 
