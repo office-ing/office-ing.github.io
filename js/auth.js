62aa7ad8a9b2c93f8d5a40a8e4d639c5db4c9980
office-ing <55531871+office-ing@users.noreply.github.com>
2024-01-24 11:20:55 +0900

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,6 @@
 (async () => {
-  const logger = "https://office-ing.net/coding/api/logger.php";
+  const logger = "https://script.google.com/macros/s/AKfycbyrSZsHxvglJp97CMsdXBl20ZVopXFOkFI1ntm7Ig0cjcGK3CT38DGR_8h1T36TiwwL/exec";
+//  const logger = "https://office-ing.net/coding/api/logger.php";
   // Host Info
   const hostinfo = {
     host: location.host,
