9889b05076f1fe96a6094d81a51bc939dde89d2b
office-ing <55531871+office-ing@users.noreply.github.com>
2024-01-25 16:14:29 +0900

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,57 +1,26 @@
-(async () => {
-    const url = "https://script.google.com/macros/s/AKfycbyrSZsHxvglJp97CMsdXBl20ZVopXFOkFI1ntm7Ig0cjcGK3CT38DGR_8h1T36TiwwL/exec";
-    const json = {
-    action: "replace",
-    sheetName: "log",
-    rows: [
+ (async () => {
+    const END_POINT = "https://script.google.com/macros/s/AKfycbyrSZsHxvglJp97CMsdXBl20ZVopXFOkFI1ntm7Ig0cjcGK3CT38DGR_8h1T36TiwwL/exec";
+    const data = {
+      action: "replace",
+      sheetName: "log",
+      rows: [
         {
-          host: location.host,
-          href: location.href,
-          pathname: location.pathname,
-          port: location.port,
-          protocol: location.protocol,
-        }
-      ]
+          URL: location.href,
+          DATE: new Date().toLocaleString(),
+        },
+      ],
     };
-    const response = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(json),
-    });
-})();
-/*
-(async () => {
-  const logger = "https://script.google.com/macros/s/AKfycbyrSZsHxvglJp97CMsdXBl20ZVopXFOkFI1ntm7Ig0cjcGK3CT38DGR_8h1T36TiwwL/exec";
-//  const logger = "https://office-ing.net/coding/api/logger.php";
-  // Host Info
-  const json = {
-    action: "replace",
-    sheetName: "log",
-    rows: [
-        {
-          host: location.host,
-          href: location.href,
-          pathname: location.pathname,
-          port: location.port,
-          protocol: location.protocol,
-        }
-      ]
-  };
-
-  // Post to server
-  const response = await fetch(logger, {
-    method: "POST",
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: JSON.stringify(json),
-  });
-
-  // // auth
-  // const result = JSON.parse(json);
-
-  // if (!result) {
-  //   document.querySelector("#app").style.visibility = "hidden";
-  // }
-})();
-*/
+    $.ajax({
+      type: "POST",
+      url: END_POINT,
+      dataType: "json",
+      data: JSON.stringify(data),
+    }).then(
+      (result) => {
+        console.log(JSON.stringify(result));
+      },
+      (error) => {
+        alert("Error:" + JSON.stringify(error));
+      }
+    );
+  })();
