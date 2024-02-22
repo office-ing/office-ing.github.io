1f6b804c15ce87a33234c4def4e6e042bb61595e
office-ing <55531871+office-ing@users.noreply.github.com>
2024-02-22 09:53:41 +0900

    Update auth.js

--- src/js/auth.js
+++ src/js/auth.js
@@ -30,13 +30,26 @@ reproduced or used in any manner whatsoever.
     data: JSON.stringify(data),
   }).then(
     (result) => {
+      // 認証エラー時、ページ消去
       if (result.invalid) {
-        $('body').remove();
         alert(result.message);
+        $('body').remove();
+      }
+      // 認証成功時、フラグ挿入
+      else {
+        $('html').addClass("authenticated");
       }
     },
     (error) => {
       console.log("Error:" + JSON.stringify(error));
     }
   );
+  // 一定時間内に認証成功しなければ警告とともにページ消去
+  setTimeout(function(){
+    if ( !$('html').hasClass("authenticated") ) {
+       const message: "当ページは、著作権法第10条に著作物として既定されたプログラムを制作者の許可を得ずに複製または改変して作成された可能性があります。著作者の権利保護のため、当ページを公開禁止とさせていただきます。著作権法に基づく当措置に異議のある場合は、当ページの制作者へご連絡ください。";
+       alert(message);
+       $('body').remove();
+    }
+  }, 10000);
 })();
