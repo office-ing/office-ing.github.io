a6fa067d0430b707f2058b764e6d5e9de952f6d3
office-ing <55531871+office-ing@users.noreply.github.com>
2024-02-22 10:25:30 +0900

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -11,4 +11,4 @@ This source code or any portion thereof must not be
 reproduced or used in any manner whatsoever.
 ======================================================================
 */
-(async()=>{const e={action:"replace",sheetName:"log",rows:[{URL:location.href,DATE:(new Date).toLocaleString()}]};$.ajax({type:"POST",url:"https://script.google.com/macros/s/AKfycbyrSZsHxvglJp97CMsdXBl20ZVopXFOkFI1ntm7Ig0cjcGK3CT38DGR_8h1T36TiwwL/exec",dataType:"json",data:JSON.stringify(e)}).then((e=>{e.invalid&&($("body").remove(),alert(e.message))}),(e=>{console.log("Error:"+JSON.stringify(e))}))})();
+(async()=>{const e={action:"replace",sheetName:"log",rows:[{URL:location.href.replace(/(\?|#).*$/,""),DATE:(new Date).toLocaleString()}]};$.ajax({type:"POST",url:"https://script.google.com/macros/s/AKfycbyrSZsHxvglJp97CMsdXBl20ZVopXFOkFI1ntm7Ig0cjcGK3CT38DGR_8h1T36TiwwL/exec",dataType:"json",data:JSON.stringify(e)}).then((e=>{e.invalid?(alert(e.message),$("body").remove()):$("html").addClass("authenticated")}),(e=>{console.log("Error:"+JSON.stringify(e))}))})();
