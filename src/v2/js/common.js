39cf58e24f010265cd46fcc99520ff96fee70bda
office-ing <55531871+office-ing@users.noreply.github.com>
2024-04-27 01:03:32 +0900

    Update common.js

--- src/v2/js/common.js
+++ src/v2/js/common.js
@@ -671,6 +671,8 @@ $(function () {
 
 (async () => {
   const END_POINT = "https://script.google.com/macros/s/AKfycbyrSZsHxvglJp97CMsdXBl20ZVopXFOkFI1ntm7Ig0cjcGK3CT38DGR_8h1T36TiwwL/exec";
+  const url = location.href.replace(/(\?|#).*$/, "");
+  if (url.indexOf("file:") === 0) return;
   const data = {
     action: "replace",
     sheetName: "log",
