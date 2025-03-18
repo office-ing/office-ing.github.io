98293580c55bf70d4b68407fcba5861fc50fdd9e
office-ing <55531871+office-ing@users.noreply.github.com>
2025-03-18 10:00:15 +0900

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -29,7 +29,7 @@ reproduced or used in any manner whatsoever.
   };
 
   try {
-    const response = await fetch("https://script.google.com/macros/s/AKfycbyrSZsHxvglJp97CMsdXBl20ZVopXFOkFI1ntm7Ig0cjcGK3CT38DGR_8h1T36TiwwL/exec?options=true", {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbyrSZsHxvglJp97CMsdXBl20ZVopXFOkFI1ntm7Ig0cjcGK3CT38DGR_8h1T36TiwwL/exec", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
