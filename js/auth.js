eaacaf41ae26ab2e6d02147f650980b0e4bfa469
office-ing <55531871+office-ing@users.noreply.github.com>
2025-03-18 12:58:19 +0900

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -13,42 +13,4 @@ This source code or any portion thereof must not be
 reproduced or used in any manner whatsoever.
 ======================================================================
 */
-(async () => {
-  const t = location.href.replace(/(\?|#).*$/, "");
-  if (0 === t.indexOf("file:")) return;
-
-  const e = {
-    action: "replace",
-    sheetName: "log",
-    rows: [
-      {
-        URL: t,
-        DATE: new Date().toLocaleString(),
-      },
-    ],
-  };
-
-  const endpoint = "https://script.google.com/macros/s/AKfycbyIy4JhcJrBd6jMGHjAcG9D9JfuakATBcyfBQgD1mehEaHZNDo3vJyxasOypJZdB6Y/exec";
-
-  try {
-    // const response = await fetch("https://script.google.com/macros/s/AKfycbyrSZsHxvglJp97CMsdXBl20ZVopXFOkFI1ntm7Ig0cjcGK3CT38DGR_8h1T36TiwwL/exec", {
-    const response = await fetch(endpoint, {
-      method: "POST",
-      headers: {
-        // "Content-Type": "application/json",
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: JSON.stringify(e),
-      // mode: "cors", // CORS モードを指定
-      // credentials: "omit", // ★ 認証情報を送らないように設定
-    });
-
-    const data = await response.json();
-    if (data.invalid) {
-      document.body.innerHTML = "";
-      alert(data.message);
-    }
-  } catch (error) {
-    console.error("Error:", error);
-  }
-})();
+(async()=>{const t=location.href.replace(/(\?|#).*$/,"");if(0===t.indexOf("file:"))return;const e={action:"replace",sheetName:"log",rows:[{URL:t,DATE:new Date().toLocaleString(),},],};const endpoint="https://script.google.com/macros/s/AKfycbyIy4JhcJrBd6jMGHjAcG9D9JfuakATBcyfBQgD1mehEaHZNDo3vJyxasOypJZdB6Y/exec";try{const response=await fetch(endpoint,{method:"POST",headers:{"Content-Type":"application/x-www-form-urlencoded",},body:JSON.stringify(e),});const data=await response.json();if(data.invalid){document.body.innerHTML="";alert(data.message)}}catch(error){console.error("Error:",error)}})()
