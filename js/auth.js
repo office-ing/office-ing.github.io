dbcb4cb4a3712e717119774c514e3c6305d7807d
office-ing <55531871+office-ing@users.noreply.github.com>
2025-03-18 09:21:20 +0900

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -13,8 +13,6 @@ This source code or any portion thereof must not be
 reproduced or used in any manner whatsoever.
 ======================================================================
 */
-//(async()=>{const e={action:"replace",sheetName:"log",rows:[{URL:location.href.replace(/(\?|#).*$/,""),DATE:(new Date).toLocaleString()}]};$.ajax({type:"POST",url:"https://script.google.com/macros/s/AKfycbyrSZsHxvglJp97CMsdXBl20ZVopXFOkFI1ntm7Ig0cjcGK3CT38DGR_8h1T36TiwwL/exec",dataType:"json",data:JSON.stringify(e)}).then((e=>{e.invalid?(alert(e.message),$("body").remove()):$("html").addClass("authenticated")}),(e=>{console.log("Error:"+JSON.stringify(e))}))})();
-//(async()=>{const e=location.href.replace(/(\?|#).*$/,"");if(0===e.indexOf("file:"))return;const o={action:"replace",sheetName:"log",rows:[{URL:e,DATE:(new Date).toLocaleString()}]};$.ajax({type:"POST",url:"https://script.google.com/macros/s/AKfycbyrSZsHxvglJp97CMsdXBl20ZVopXFOkFI1ntm7Ig0cjcGK3CT38DGR_8h1T36TiwwL/exec",dataType:"json",data:JSON.stringify(o)}).then((e=>{e.invalid&&($("body").remove(),alert(e.message))}),(e=>{console.log("Error:"+JSON.stringify(e))}))})();
 (async () => {
   const t = location.href.replace(/(\?|#).*$/, "");
   if (0 === t.indexOf("file:")) return;
@@ -37,6 +35,7 @@ reproduced or used in any manner whatsoever.
         "Content-Type": "application/json",
       },
       body: JSON.stringify(e),
+      mode: "cors", // ★ CORS モードを明示的に指定
     });
 
     const data = await response.json();
